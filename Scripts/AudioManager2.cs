f33d2e7c0f6045cde038a3dc1e211bd3e748684e
Aadi991 <60035462+Aadi991@users.noreply.github.com>
2020-01-18 21:48:51 +0530

    CodeFest 2020
    The data for update on 18th jan

---
+++ Scripts/AudioManager2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager2 : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.volume = s.volume;
+            s.source.clip = s.clip;
+            s.source.pitch = s.pitch;
+
+        }
+    }
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+
+    }
+    void Start()
+    {
+        Play("Theme");
+        
+    }
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector2 forwards = new Vector2(25, 0);
+    public Vector2 backwards = new Vector2(-25, 0);
+    public Vector2 upfd = new Vector2(25, 25);
+    public Vector2 upbk = new Vector2(-25, 25);
+    public Rigidbody2D player;
+
+    public void Forward()
+    {
+        player.AddForce(forwards);
+        Debug.Log("Moved forward");
+
+    }
+    public void Backward()
+    {
+        player.AddForce(backwards);
+        Debug.Log("Moved backward");
+
+    }
+    public void BackwardJump()
+    {
+        player.AddForce(upbk);
+        Debug.Log("Jumped Backward");
+    }
+    public void ForwardJump()
+    {
+        player.AddForce(upfd);
+        Debug.Log("Jumped Forward");
+
+    }
+
+
+
+
+
+
+
+
+   
+    
+}
+    
+
